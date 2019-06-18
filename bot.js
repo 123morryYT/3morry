aa985528a87d2bac0d6307cc7816c49f3105d454
123morryYT <44780155+123morryYT@users.noreply.github.com>
2019-06-18 16:56:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -126,26 +126,6 @@ client.on('message', message => {//new msg event
 
 
 
-client.on('guildMemberAdd', member => {
-
-  member.guild.fetchInvites().then(guildInvites => {
-
-    const ei = invites[member.guild.id];
-
-    const invite = guildInvites.find(i =>ei.get(i.code).uses < i.uses);
-
-    const inviter =client.users.get(invite.inviter.id);
-
-    const xkiller = member.guild.channels.find("name","welcome");
-
-     xkiller.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
-
-  });
-
-})
-
-
-
 
 
 
